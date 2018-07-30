4a7a5c8d4e0904a980482ada0c41d41ae35ea45c
Alec Kucala <kucala@Alecs-MacBook-Pro.local>
2018-07-29 22:20:46 -0600

    hello world test

---
+++ main.C
@@ -0,0 +1,17 @@
+/*
+ * main.C
+ *
+ *  Created on: Jul 29, 2018
+ *      Author: kucala
+ */
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "hello world" << endl;
+
+  return 0;
+}
